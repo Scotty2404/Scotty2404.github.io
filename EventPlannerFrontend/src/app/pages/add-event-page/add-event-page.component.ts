dbcc8cf3cb7c80d6ab63c981eed903b7e0f4e22b
Fabienne Faupel <l.faupel@maximator-hydrogen.de>
2025-03-27 08:30:44 +0100

    add-event-page Formular fertig (umfrage fehlt noch)

--- EventPlannerFrontend/src/app/pages/add-event-page/add-event-page.component.ts
+++ EventPlannerFrontend/src/app/pages/add-event-page/add-event-page.component.ts
@@ -11,6 +11,7 @@ import { MatRadioModule } from '@angular/material/radio';
 import { MatButtonModule } from '@angular/material/button';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatRadioButton } from '@angular/material/radio';
+import { MatIcon } from '@angular/material/icon';
 
 
 
@@ -27,7 +28,8 @@ import { MatRadioButton } from '@angular/material/radio';
     MatRadioModule,
     MatButtonModule,
     ReactiveFormsModule,
-    MatRadioButton
+    MatRadioButton,
+    MatIcon
   ],
   templateUrl: './add-event-page.component.html',
   styleUrl: './add-event-page.component.scss'
@@ -35,6 +37,9 @@ import { MatRadioButton } from '@angular/material/radio';
 export class AddEventPageComponent {
   eventForm: FormGroup;
   selectedFile: File | null = null;
+  previewImage: string | null = null;
+  
+  
   
 
   standardImages = [
@@ -48,7 +53,9 @@ export class AddEventPageComponent {
     this.eventForm = this.fb.group({
       title: ['', Validators.required],
       date: ['', Validators.required],
-      location: ['', Validators.required],
+      street: ['', Validators.required], 
+      city: ['', Validators.required], 
+      postalCode: ['', [Validators.required, Validators.pattern('^[0-9]{5}$')]],
       description: ['', Validators.required],
       image: ['', Validators.required],
       guestCount: [''],
@@ -81,7 +88,14 @@ this.eventForm.get('timeOption')?.valueChanges.subscribe((value) => {
     const file = event.target.files[0];
     if (file) {
       this.selectedFile = file;
-      this.eventForm.patchValue({ image: file.name });
+      const reader = new FileReader();
+      
+      reader.onload = (e: any) => {
+        this.previewImage = e.target.result; // Bild als Base64 speichern
+        this.eventForm.patchValue({ image: this.previewImage }); // Bild direkt auswählen
+      };
+      
+      reader.readAsDataURL(file);
     }
   }
 
