f56832f168f745486517c4d895fa93aa3e857f66
Fabienne Faupel <l.faupel@maximator-hydrogen.de>
2025-04-23 13:57:39 +0200

    qr page mit dialog fenster

--- EventPlannerFrontend/src/app/pages/qr-event-page/qr-event-page.component.ts
+++ EventPlannerFrontend/src/app/pages/qr-event-page/qr-event-page.component.ts
@@ -6,6 +6,8 @@ import { MatButtonModule } from '@angular/material/button';
 import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
 import { RouterLink } from '@angular/router';
+import { QrDialogComponent } from '../../components/qr-dialog/qr-dialog.component';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-qr-event-page',
@@ -16,14 +18,16 @@ import { RouterLink } from '@angular/router';
     MatButtonModule,
     CommonModule,
     MatCardModule,
-    RouterLink
+    RouterLink,
+    
+
   ],
   templateUrl: './qr-event-page.component.html',
   styleUrl: './qr-event-page.component.scss'
 })
 export class QrEventPageComponent {
   attending: 'yes' | 'no' | null = null;
-  surveyAvailable = true; // ← vom Backend setzen, falls es eine Umfrage gibt
+  surveyAvailable = true;
   responseSubmitted = false;
   responseMessage = '';
 
@@ -46,15 +50,23 @@ export class QrEventPageComponent {
     info: 'Ein gemütliches Beisammensein mit Essen, Musik und Spielen.'
   };
 
+  constructor(private dialog: MatDialog) {}
+
   submitResponse() {
-    if (this.attending === 'yes') {
-      console.log('Zusage:', this.guest);
-      this.responseMessage = 'Du hast erfolgreich zugesagt!';
-    } else if (this.attending === 'no') {
-      console.log('Absage');
-      this.responseMessage = 'Deine Absage wurde gespeichert.';
-    }
-    alert('Antwort wurde übermittelt. Danke!');
+    const isYes = this.attending === 'yes';
+    const message = isYes ? 'Du hast erfolgreich zugesagt!' : 'Deine Absage wurde erfolgreich zugeschickt.';
+  
+    this.dialog.open(QrDialogComponent, {
+      data: {
+        title: isYes ? '🎉 Zusage gespeichert' : '❌ Absage gespeichert',
+        message: message,
+        attending: this.attending,
+        surveyAvailable: this.surveyAvailable
+      }
+    });
+  
     this.responseSubmitted = true;
+    this.responseMessage = message;
   }
+  
 }
