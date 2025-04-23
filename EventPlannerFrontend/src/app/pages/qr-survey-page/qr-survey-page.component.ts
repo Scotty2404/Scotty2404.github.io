9bd21e18d57e7f0853eca1b775891c5e17a11943
Fabienne Faupel <l.faupel@maximator-hydrogen.de>
2025-04-23 15:15:40 +0200

    qr code umfrage grundkonzept fertiggestellt

--- EventPlannerFrontend/src/app/pages/qr-survey-page/qr-survey-page.component.ts
+++ EventPlannerFrontend/src/app/pages/qr-survey-page/qr-survey-page.component.ts
@@ -3,7 +3,7 @@ import { FormsModule } from '@angular/forms';  // Für ngModel und die Bindung v
 import { MatFormFieldModule } from '@angular/material/form-field';  // Für das MatFormField
 import { MatInputModule } from '@angular/material/input';  // Für Input-Elemente
 import { MatSelectModule } from '@angular/material/select';  // Für Select-Elemente
-import { MatSliderModule } from '@angular/material/slider';  // Für den Slider
+
 import { MatCardModule } from '@angular/material/card';  // Für Card-Komponenten
 import { MatButtonModule } from '@angular/material/button';
 import { CommonModule } from '@angular/common';
@@ -11,7 +11,8 @@ import { FormBuilder, FormGroup, FormArray, FormControl } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatDividerModule } from '@angular/material/divider';
-import { MatSlider } from '@angular/material/slider';
+import { MatSliderModule } from '@angular/material/slider';
+
 
 
 @Component({
@@ -28,35 +29,38 @@ import { MatSlider } from '@angular/material/slider';
     ReactiveFormsModule,
     MatCheckboxModule,
     MatDividerModule,
-    MatSlider
   ],
   templateUrl: './qr-survey-page.component.html',
   styleUrl: './qr-survey-page.component.scss'
 })
 export class QrSurveyPageComponent {
   checkboxOptions = ['Option A', 'Option B', 'Option C'];
+
+  disabled = false;
+  max = 100;
+  min = 0;
+  step = 1;
+  value = 0;
+
   surveyForm: FormGroup;
+
+  get checkboxes(): FormArray<FormControl> {
+    return this.surveyForm.get('checkboxes') as FormArray<FormControl>;
+  }
 
   constructor(private fb: FormBuilder) {
     this.surveyForm = this.fb.group({
-      checkboxQuestion: this.fb.array(this.checkboxOptions.map(() => this.fb.control(false))),
+      checkboxes: this.fb.array(this.checkboxOptions.map(() => false)),
       openAnswer: [''],
-      scale: [null]
+      scale: new FormControl(this.value) // <- das hier fehlt!
     });
+    
   }
-
-  get checkboxFormArray(): FormArray {
-    return this.surveyForm.get('checkboxQuestion') as FormArray;
+  onSubmit() {
+    const rawValues = this.surveyForm.value;
+    const selectedOptions = this.checkboxOptions.filter((option, i) => rawValues.checkboxes[i]);
+    console.log('Ausgewählte Optionen:', selectedOptions);
+    console.log('Offene Antwort:', rawValues.openAnswer);
+    console.log('Skalenwert:', rawValues.scale);
   }
-
-  getCheckboxControl(i: number): FormControl {
-    return this.checkboxFormArray.at(i) as FormControl;
-  }
-  
-  
-  onSubmit(): void {
-    console.log(this.surveyForm.value);
-  }
-
-  
 }
