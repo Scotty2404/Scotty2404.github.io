24cf019854035e50c1aa09fb134a3cc205b080e1
Fabienne Faupel <l.faupel@maximator-hydrogen.de>
2025-04-24 10:31:13 +0200

    qr survey dialog component hinzugefügt

--- EventPlannerFrontend/src/app/pages/qr-survey-page/qr-survey-page.component.ts
+++ EventPlannerFrontend/src/app/pages/qr-survey-page/qr-survey-page.component.ts
@@ -12,6 +12,9 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatSliderModule } from '@angular/material/slider';
+import { Validators, ValidatorFn, AbstractControl, ValidationErrors } from '@angular/forms';
+import { MatDialog } from '@angular/material/dialog';
+import { QrSurveyDialogComponent } from '../../components/qr-survey-dialog/qr-survey-dialog.component';
 
 
 
@@ -29,6 +32,7 @@ import { MatSliderModule } from '@angular/material/slider';
     ReactiveFormsModule,
     MatCheckboxModule,
     MatDividerModule,
+    QrSurveyDialogComponent
   ],
   templateUrl: './qr-survey-page.component.html',
   styleUrl: './qr-survey-page.component.scss'
@@ -44,23 +48,44 @@ export class QrSurveyPageComponent {
 
   surveyForm: FormGroup;
 
+  constructor(private fb: FormBuilder, private dialog: MatDialog) {
+    this.surveyForm = this.fb.group({
+      checkboxes: this.fb.array(
+        this.checkboxOptions.map(() => this.fb.control(false)),
+        [this.singleCheckboxSelectedValidator()]
+      ),
+      openAnswer: ['', Validators.required],
+      scale: [null, Validators.required]
+    });
+  }
+
   get checkboxes(): FormArray<FormControl> {
     return this.surveyForm.get('checkboxes') as FormArray<FormControl>;
   }
 
-  constructor(private fb: FormBuilder) {
-    this.surveyForm = this.fb.group({
-      checkboxes: this.fb.array(this.checkboxOptions.map(() => false)),
-      openAnswer: [''],
-      scale: new FormControl(this.value) // <- das hier fehlt!
+  onSingleCheckboxChange(selectedIndex: number): void {
+    this.checkboxes.controls.forEach((ctrl, i) => {
+      ctrl.setValue(i === selectedIndex);
     });
-    
+    // Trigger Validator Update
+    this.checkboxes.updateValueAndValidity();
   }
+
+  singleCheckboxSelectedValidator(): ValidatorFn {
+    return (formArray: AbstractControl): ValidationErrors | null => {
+      const selected = (formArray as FormArray).controls.filter(ctrl => ctrl.value);
+      return selected.length === 1 ? null : { notOneSelected: true };
+    };
+  }
+
   onSubmit() {
+    if (this.surveyForm.invalid) return;
+
     const rawValues = this.surveyForm.value;
     const selectedOptions = this.checkboxOptions.filter((option, i) => rawValues.checkboxes[i]);
-    console.log('Ausgewählte Optionen:', selectedOptions);
+    console.log('Ausgewählte Option:', selectedOptions);
     console.log('Offene Antwort:', rawValues.openAnswer);
     console.log('Skalenwert:', rawValues.scale);
+    this.dialog.open(QrSurveyDialogComponent);
   }
 }
